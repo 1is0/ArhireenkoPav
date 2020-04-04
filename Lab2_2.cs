9892ce8fd394123faf997966ba48a7a96ccd68fc
TheLostLegend <61868705+TheLostLegend@users.noreply.github.com>
2020-04-04 23:19:53 +0300

    Update Lab2_2.cs

--- Lab2_2.cs
+++ Lab2_2.cs
@@ -1,32 +1,38 @@
-﻿using System;
+using System;
 using System.Text;
 using System.Globalization;
 
 namespace Example
 {
-    class Program
+    class Lab2_2
     {
 
         static void Main()
         {
-            int k=0, l=0, i, m=0, start=0, j;
-            StringBuilder text = new StringBuilder(" И поэтому все так произошло"); //пробел перед первым символом строки обязателен
+            int k=1, l=0, i, start=0, j;
+            StringBuilder text = new StringBuilder("И поэтому все так произошло");
             for (i = 0; i< text.Length; i++)        
             {
                 if (text[i].Equals(' ')) k++;
             }
-            StringBuilder words1 = new StringBuilder("                             "); //кол-во пробелов = кол-ву символов в изначальной строке, можно больше
+            Console.WriteLine(text.Length);
+            char[] words1 = new char[text.Length];
             for (i = text.Length - 1; i >=0 ; i--)
             {
-                if (i == text.Length-1 || text[i + 1].Equals(' ')) start = i;
+                if (i == text.Length-1 || text[i+1].Equals(' ')) start = i;
                 if (text[i].Equals(' ')||i==0)
                 {
-                    for (j = i; j < start+1; j++)
+                    for (j = i; j <= start; j++)
                     {
+                        if (text[j].Equals(' ')) j++;
                         words1[l] = text[j];
                         l++;
                     }
-                    
+                    if (i != 0)
+                    {
+                        words1[l] = ' ';
+                        l++;
+                    }
                 }
             }
             Console.WriteLine(words1); 
