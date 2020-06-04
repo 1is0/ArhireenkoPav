cca7ffca6733970fbb2de08d2af257d40971ee9a
TheLostLegend <61868705+TheLostLegend@users.noreply.github.com>
2020-06-04 20:15:23 +0300

    Add files via upload

---
+++ C# Labs 3-8/Lab 4/Lab4_1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnusualPlayer1
+{
+    public partial class Form1 : Form
+    {
+        private UnusualPlayer _player;
+        public Form1()
+        {
+          InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog file1 = new OpenFileDialog())
+            {
+                file1.Filter = "Mp3 File|*.mp3";
+                file1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+                if (file1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    _player = new UnusualPlayer(file1.FileName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_player != null)
+                _player.Play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_player != null)
+                _player.Dispose();
+        }
+    }
+}
---
+++ C# Labs 3-8/Lab 4/Lab4_1/WindowsFormsApp1/UnusualPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnusualPlayer1
+{
+    class UnusualPlayer : IDisposable
+    {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        public UnusualPlayer(string fileName)
+        {
+            const string form = @"open ""{0}"" type mpegvideo alias MediaFile";
+            string Command = String.Format(form, fileName);
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+        public void Play()
+        {
+            string Command = "play MediaFile";
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+        public void Dispose()
+        {
+            string Command = "close MediaFile";
+            mciSendString(Command, null, 0, IntPtr.Zero);
+        }
+    }
+}
