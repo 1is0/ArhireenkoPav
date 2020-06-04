0a1686a84c805c6105f0128da8d30f2958116a07
TheLostLegend <61868705+TheLostLegend@users.noreply.github.com>
2020-06-04 20:32:32 +0300

    Add files via upload

---
+++ C# Labs 3-8/Lab 7/Lab7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LR7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RatNum a, b;
+
+            Console.WriteLine("Введите 2 части рационального числа: ");
+            var forNum = int.Parse(Console.ReadLine());
+            var forDenum = int.Parse(Console.ReadLine());
+            a = new RatNum(forNum, forDenum);
+
+            Console.WriteLine("И так же для второго числа: ");
+            forNum = int.Parse(Console.ReadLine());
+            forDenum = int.Parse(Console.ReadLine());
+            b = new RatNum(forNum, forDenum);
+
+            Console.WriteLine("Приведение к целым и действительным числам первого рационального числа:");
+            int ires = (int)a;
+            Console.WriteLine($"Приведение к целым:{ires}");
+            double dres = (double)a;
+            Console.WriteLine($"Приведение к действительным:{dres}");
+
+            Console.WriteLine("A + B = {0}\n", (a + b).ToString());
+            Console.WriteLine("A - B = {0}\n", (a - b).ToString());
+            Console.WriteLine("A * B = {0}\n", (a * b).ToString());
+            Console.WriteLine("A / B = {0}\n", (a / b).ToString());
+            Console.WriteLine(a.Equals(b));
+
+            Console.WriteLine("Сравнение:");
+            if (a > b)
+            {
+                Console.WriteLine("Первое больше второго.");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("Второе больше первого.");
+            }
+            else
+            {
+                Console.WriteLine("Они равны.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C# Labs 3-8/Lab 7/Lab7/RatNum.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LR7
+{
+    public class RatNum : IComparable<RatNum>, IEquatable<RatNum>
+    {
+        //       whole/natural        //
+        private readonly int whole;
+        private readonly int natural;
+
+        public RatNum(int new_whole, int new_natural)
+        {
+            whole = new_whole;
+            natural = new_natural;
+        }
+
+        ~RatNum() { }
+
+        public static RatNum operator +(RatNum n1, RatNum n2)
+        {
+            RatNum result;
+            if (n1.natural == n2.natural)
+                result = new RatNum(n1.whole + n2.whole, n1.natural);
+            else
+                result = new RatNum((n1.whole * n2.natural) + (n2.whole * n1.natural), n1.natural * n2.natural);
+
+            return result;
+        }
+
+        public static RatNum operator -(RatNum n1, RatNum n2)
+        {
+            RatNum result;
+            if (n1.natural == n2.natural)
+                result = new RatNum(n1.whole - n2.whole, n1.natural);
+            else
+                result = new RatNum((n1.whole * n2.natural) - (n1.natural * n2.whole), n1.natural * n2.natural);
+            return result;
+        }
+
+        public static RatNum operator *(RatNum n1, RatNum n2)
+        {
+            RatNum result = new RatNum(n1.whole * n2.whole, n1.natural * n2.natural);
+            return result;
+        }
+
+        public static RatNum operator /(RatNum n1, RatNum n2)
+        {
+            RatNum result = new RatNum(n1.whole * n2.natural, n1.natural * n2.whole);
+            return result;
+        }
+
+
+        public static bool operator >(RatNum n1, RatNum n2)
+        {
+                RatNum res1 = new RatNum(n1.whole * n2.natural, n1.natural * n2.natural);
+                RatNum res2 = new RatNum(n2.whole * n1.natural, n1.natural * n2.natural);
+                if (res1.whole > res2.whole) return true;
+                else return false;
+            
+        }
+
+        public static bool operator <(RatNum n1, RatNum n2)
+        {
+                RatNum res1 = new RatNum(n1.whole * n2.natural, n1.natural * n2.natural);
+                RatNum res2 = new RatNum(n2.whole * n1.natural, n1.natural * n2.natural);
+                if (res1.whole < res2.whole) return true;
+                else return false;
+        }
+
+        public static bool operator >=(RatNum n1, RatNum n2)
+        {
+                RatNum res1 = new RatNum(n1.whole * n2.natural, n1.natural * n2.natural);
+                RatNum res2 = new RatNum(n2.whole * n1.natural, n1.natural * n2.natural);
+                if (res1.whole >= res2.whole) return true;
+                else return false;
+
+        }
+
+        public static bool operator <=(RatNum n1, RatNum n2)
+        {
+                RatNum res1 = new RatNum(n1.whole * n2.natural, n1.natural * n2.natural);
+                RatNum res2 = new RatNum(n2.whole * n1.natural, n1.natural * n2.natural);
+                if (res1.whole <= res2.whole) return true;
+                else return false;
+        }
+
+
+
+
+        //Convertion
+        public override string ToString()
+        {
+            return $"{whole}/{natural}";
+        }
+
+        //Eq check
+        public bool Equals(RatNum nZ)
+        {
+            if (ReferenceEquals(null, nZ)) return false;
+            if (ReferenceEquals(this, nZ)) return true;
+            return whole == nZ.whole && natural == nZ.natural;
+        }
+
+        public int CompareTo(RatNum nZ)
+        {
+            if (this.whole * nZ.natural < nZ.whole * this.natural)
+            {
+                return 1;
+            }
+            else if (this.whole * nZ.natural > nZ.whole * this.natural)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static implicit operator int(RatNum nZ)
+        {
+            return (nZ.whole / nZ.natural);
+        }
+
+        public static implicit operator double(RatNum nZ)
+        {
+            return (double)nZ.whole / nZ.natural;
+        }
+    }
+}
