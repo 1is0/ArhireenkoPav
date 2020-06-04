55caa265e45744eb51ba01776240b81be09d5e02
TheLostLegend <61868705+TheLostLegend@users.noreply.github.com>
2020-06-04 20:22:39 +0300

    Add files via upload

---
+++ C# Labs 3-8/Lab 4/Lab4_2/Lab4_2/Lab4_x.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace L4_2
+{
+    class Program
+    {
+        [DllImport("Project1.dll")]
+        public static extern void write();
+        static void Main(string[] args)
+        {
+            write();
+        }
+    }
+}
